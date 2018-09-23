b8cc5737aca09228d22a6acecfc4110d057b8c7e
BradleyWills <43045874+BradleyWills@users.noreply.github.com>
2018-09-23 19:45:36 -0400

    Update Contents.swift

--- Unit 1-02.playground/Contents.swift
+++ Unit 1-02.playground/Contents.swift
@@ -1,48 +1,14 @@
-// Created on: Sept-2018
-// Created by: Ethan Bellem
-// Created for: ICS3U
-//  will display a picuture
-// this will be commented out when code moved to Xcode
-// this will be commented out when code moved to Xcode
-import PlaygroundSupport
+// made by Bradley Wills
+// made on Sept 21, 2018
+// made for: ICS3U
+// made to calculate the circumference of a circle
 
+show("What is the radius?")
 
-import UIKit
+let PI : Float = 3.14
 
-class ViewController : UIViewController {
-    // this is the main view controller, that will show the UIKit elements
-    
-    // properties
-    //let image = UIImage(named: "IMG_1612.JPG")
-    let imageView2 = UIImageView(image: UIImage(named: "IMG_1964.PNG"))
-    let imageView = UIImageView(image: UIImage(named: "IMG_1853.PNG"))
-    
-    override func viewDidLoad() {
-        // UI
-        super.viewDidLoad()
-        
-        let view = UIView()
-        view.backgroundColor = #colorLiteral(red: 0.239215686917305, green: 0.674509823322296, blue: 0.968627452850342, alpha: 1.0)
-        self.view = view
-        
-        
-        view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.leadingAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-        view.addSubview(imageView2)
-        imageView2.translatesAutoresizingMaskIntoConstraints = false
-        imageView2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView2.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-    }
-    
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-}
+let radius = Float(askForNumber("Radius"))
 
-// this will be commented out when code moved to Xcode
-PlaygroundPage.current.liveView = ViewController()
+let circumferenceAnswer = PI * (radius * 2)
+
+show("The circumference is: " + String(circumferenceAnswer) + "cm")
